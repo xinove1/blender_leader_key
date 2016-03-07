bb8fd440b7a33645fb4ee93f5e2166f4c02e1ce0
Adam Wolski <miniukof@gmail.com>
2016-03-07 09:40:26 +0100

    Working, needs better configuration options

---
+++ bl_leader_key.py
@@ -0,0 +1,226 @@
+"""
+File: bl_leader_key.py
+Author: Adam Wolski
+Email: miniukof@gmail.com
+Github: https://github.com/miniukof
+"""
+from time import time
+import bpy
+from bpy.types import Operator, AddonPreferences
+from bpy.props import StringProperty, IntProperty, BoolProperty
+
+bl_info = {
+    'name'        : 'Leader Key',
+    'description' : 'Vim like <leader> key functionality for blender',
+    'author'      : 'miniukof',
+    'version'     : (0, 0, 1),
+    'blender'     : (2, 76, 11),
+    'category'    : 'User Interface',
+}
+
+# TODO Make those availble to setup in preferences
+TIMEOUT = 1
+WAIT_FOR_NEXT = 0.5
+BINDINGS_NUMBER = 5
+
+class LeaderKey(bpy.types.Operator):
+    """Main function call"""
+    bl_idname = 'ui.leaderkey'
+    bl_label = 'Launch leader key'
+
+    def __init__(self):
+        self.context_type = ''
+        self.context_mode = ''
+        self.key_string = ''
+        self.timeout = 0
+        self.bindings = Bindings()
+
+    def invoke(self, context, event):
+        user_preferences = context.user_preferences
+        addon_prefs = user_preferences.addons[__name__].preferences
+
+        self.bindings = Bindings()
+        for i in range(addon_prefs.bindings_number):
+            exec('self.bindings.append({0}.kstr{1},\
+                    {0}.func{1}, {0}.ctype{1}, {0}.cmode{1})'.format(
+                        'addon_prefs', i))
+
+        self.context_type = context.area.type
+        self.context_mode = context.mode
+        self.key_string = ''
+        self.timeout = time()
+        context.window_manager.modal_handler_add(self)
+        return {'RUNNING_MODAL'}
+
+    def modal(self, context, event):
+        self.context_type = context.area.type
+        self.context_mode = context.mode
+
+        if event.type in EVENTS and event.value == 'PRESS':
+            self.key_string += event.type + ' '
+
+        func = self.bindings.get(kstr=self.key_string,
+                                 ctype=self.context_type,
+                                 cmode=self.context_mode)
+        if not func:
+            func = self.bindings.get(kstr=self.key_string,
+                                     ctype='',
+                                     cmode=self.context_mode)
+        if not func:
+            func = self.bindings.get(kstr=self.key_string,
+                                     ctype=self.context_type,
+                                     cmode='')
+        if not func:
+            func = self.bindings.get(kstr=self.key_string,
+                                     ctype='',
+                                     cmode='')
+
+        if func and time() - self.timeout >= WAIT_FOR_NEXT:
+            exec(func)
+            return {'FINISHED'}
+
+        if time() - self.timeout >= TIMEOUT:
+            return {'FINISHED'}
+
+        return {'RUNNING_MODAL'}
+
+
+class Bindings:
+    """Class containing all the bindings."""
+    _bindings = {}
+
+    def append(self,
+               kstr,
+               func,
+               ctype='',
+               cmode=''):
+        """
+        Append binding for key combination string 'kstr' and bind it with
+        function 'func'.
+        Constrain that binding to context_area_type 'ctype' and context_mode
+        'cmode'
+        """
+        # Blenders event.type has all the keys in upper case.
+        kstr = kstr.upper().strip()
+        if self._bindings.get(ctype):
+            if self._bindings[ctype].get(cmode):
+                self._bindings[ctype][cmode][kstr] = func
+            else:
+                self._bindings[ctype][cmode] = {kstr: func}
+        else:
+            self._bindings[ctype] = {cmode: {kstr: func}}
+
+    def get(self, kstr, ctype='', cmode=''):
+        """
+        Get function for key_string 'kstr' bound by context type and mode:
+        'ctype', 'cmode'
+        """
+        try:
+            return self._bindings[ctype][cmode][kstr.upper().strip()]
+        except KeyError:
+            return None
+
+class LeaderKeyPreferences(AddonPreferences):
+    bl_idname = __name__
+
+
+    keystr_desc = ('String of characters to be used as key binding. For ex: '
+                   '"A B C" would mean that after pressing leader key pressing in '
+                   'A, B, C keys in sequence would lead to calling bound function.')
+
+    func_desc = 'Function to be used by this key binding'
+
+    ctype_desc = ('Type of area in which this key binding is to be used in for ex:'
+                  ' "3D_VIEW". Can be left empty, in which case this key binding'
+                  ' would not be dependent on current area. \nAvailible types:'
+                  ' ‘VIEW_3D’, ‘TIMELINE’, ‘GRAPH_EDITOR’, ‘DOPESHEET_EDITOR’,'
+                  ' ‘NLA_EDITOR’, ‘IMAGE_EDITOR’, ‘SEQUENCE_EDITOR’, ‘CLIP_EDITOR’,'
+                  ' ‘TEXT_EDITOR’, ‘NODE_EDITOR’, ‘LOGIC_EDITOR’, ‘PROPERTIES’,'
+                  ' ‘OUTLINER’, ‘USER_PREFERENCES’, ‘INFO’, ‘FILE_BROWSER’,'
+                  ' ‘CONSOLE’')
+
+    cmode_desc = ('Mode in which this key binding is to be used in for ex: '
+                  '"EDIT_MESH". Can be left empty, in which case this key binding'
+                  'would not be dependent on current mode. \nAvailible modes:'
+                  ' ‘EDIT_MESH’, ‘EDIT_CURVE’, ‘EDIT_SURFACE’, ‘EDIT_TEXT’,'
+                  ' ‘EDIT_ARMATURE’, ‘EDIT_METABALL’, ‘EDIT_LATTICE’, ‘POSE’,'
+                  ' ‘SCULPT’, ‘PAINT_WEIGHT’, ‘PAINT_VERTEX’, ‘PAINT_TEXTURE’,'
+                  ' ‘PARTICLE’, ‘OBJECT’')
+
+    bindnum = BINDINGS_NUMBER
+
+    for i in range(bindnum):
+        exec('kstr{} = StringProperty(name="Key string",\
+                                      description=keystr_desc)'.format(i))
+        exec('func{} = StringProperty(name="Function",\
+                                      description=func_desc)'.format(i))
+        exec('ctype{} = StringProperty(name="Context area type",\
+                                       description=ctype_desc)'.format(i))
+        exec('cmode{} = StringProperty(name="Context mode",\
+                                       description=ctype_desc)'.format(i))
+
+    def draw(self, context):
+        layout = self.layout
+        layout.label(text='Leader Key Preferences')
+        for i in range(self.bindnum):
+            layout.label(text="Binding {}".format(i))
+            layout.prop(self, "kstr{}".format(i))
+            layout.prop(self, "func{}".format(i))
+            layout.prop(self, "ctype{}".format(i))
+            layout.prop(self, "cmode{}".format(i))
+
+
+
+CTYPE_ENUM = ['VIEW_3D', 'TIMELINE', 'GRAPH_EDITOR', 'DOPESHEET_EDITOR',
+              'NLA_EDITOR', 'IMAGE_EDITOR', 'SEQUENCE_EDITOR', 'CLIP_EDITOR',
+              'TEXT_EDITOR', 'NODE_EDITOR', 'LOGIC_EDITOR', 'PROPERTIES',
+              'OUTLINER', 'USER_PREFERENCES', 'INFO', 'FILE_BROWSER',
+              'CONSOLE']
+
+CMODE_ENUM = ['EDIT_MESH', 'EDIT_CURVE', 'EDIT_SURFACE', 'EDIT_TEXT',
+              'EDIT_ARMATURE', 'EDIT_METABALL', 'EDIT_LATTICE', 'POSE',
+              'SCULPT', 'PAINT_WEIGHT', 'PAINT_VERTEX', 'PAINT_TEXTURE',
+              'PARTICLE', 'OBJECT']
+
+EVENTS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+          'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'ZERO',
+          'ONE', 'TWO', 'THREE', 'FOUR', 'FIVE', 'SIX', 'SEVEN', 'EIGHT',
+          'NINE', 'LEFT_CTRL', 'LEFT_ALT', 'LEFT_SHIFT', 'RIGHT_ALT',
+          'RIGHT_CTRL', 'RIGHT_SHIFT', 'OSKEY', 'GRLESS', 'ESC', 'TAB', 'RET',
+          'SPACE', 'LINE_FEED', 'BACK_SPACE', 'DEL', 'SEMI_COLON', 'PERIOD',
+          'COMMA', 'QUOTE', 'ACCENT_GRAVE', 'MINUS', 'SLASH', 'BACK_SLASH',
+          'EQUAL', 'LEFT_BRACKET', 'RIGHT_BRACKET', 'LEFT_ARROW', 'DOWN_ARROW',
+          'RIGHT_ARROW', 'UP_ARROW', 'NUMPAD_2', 'NUMPAD_4', 'NUMPAD_6',
+          'NUMPAD_8', 'NUMPAD_1', 'NUMPAD_3', 'NUMPAD_5', 'NUMPAD_7',
+          'NUMPAD_9', 'NUMPAD_PERIOD', 'NUMPAD_SLASH', 'NUMPAD_ASTERIX',
+          'NUMPAD_0', 'NUMPAD_MINUS', 'NUMPAD_ENTER', 'NUMPAD_PLUS', 'F1',
+          'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12',
+          'F13', 'F14', 'F15', 'F16', 'F17', 'F18', 'F19', 'PAUSE', 'INSERT',
+          'HOME', 'PAGE_UP', 'PAGE_DOWN', 'END', 'MEDIA_PLAY', 'MEDIA_STOP',
+          'MEDIA_FIRST', 'MEDIA_LAST', 'TEXTINPUT', 'WINDOW_DEACTIVATE',
+          'TIMER', 'TIMER0', 'TIMER1', 'TIMER2', 'TIMER_JOBS',
+          'TIMER_AUTOSAVE', 'TIMER_REPORT', 'TIMERREGION', 'NDOF_MOTION',
+          'NDOF_BUTTON_MENU', 'NDOF_BUTTON_FIT', 'NDOF_BUTTON_TOP',
+          'NDOF_BUTTON_BOTTOM', 'NDOF_BUTTON_LEFT', 'NDOF_BUTTON_RIGHT',
+          'NDOF_BUTTON_FRONT', 'NDOF_BUTTON_BACK', 'NDOF_BUTTON_ISO1',
+          'NDOF_BUTTON_ISO2', 'NDOF_BUTTON_ROLL_CW', 'NDOF_BUTTON_ROLL_CCW',
+          'NDOF_BUTTON_SPIN_CW', 'NDOF_BUTTON_SPIN_CCW', 'NDOF_BUTTON_TILT_CW',
+          'NDOF_BUTTON_TILT_CCW', 'NDOF_BUTTON_ROTATE', 'NDOF_BUTTON_PANZOOM',
+          'NDOF_BUTTON_DOMINANT', 'NDOF_BUTTON_PLUS', 'NDOF_BUTTON_MINUS',
+          'NDOF_BUTTON_ESC', 'NDOF_BUTTON_ALT', 'NDOF_BUTTON_SHIFT',
+          'NDOF_BUTTON_CTRL', 'NDOF_BUTTON_1', 'NDOF_BUTTON_2',
+          'NDOF_BUTTON_3', 'NDOF_BUTTON_4', 'NDOF_BUTTON_5', 'NDOF_BUTTON_6',
+          'NDOF_BUTTON_7', 'NDOF_BUTTON_8', 'NDOF_BUTTON_9', 'NDOF_BUTTON_10',
+          'NDOF_BUTTON_A', 'NDOF_BUTTON_B', 'NDOF_BUTTON_C']
+
+def register():
+    bpy.utils.register_class(LeaderKey)
+    bpy.utils.register_class(LeaderKeyPreferences)
+
+def unregister():
+    bpy.utils.unregister_class(LeaderKey)
+    bpy.utils.unregister_class(LeaderKeyPreferences)
+
+if __name__ == '__main__':
+    unregister()
+    register()
